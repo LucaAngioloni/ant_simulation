9f5b5c4cc565b66c40ed7040ef118165a25ff015
Luca Angioloni <lucaangioloni@gmail.com>
2021-06-13 02:12:48 +0200

    added first implementation of pheromones

--- js/ant.js
+++ js/ant.js
@@ -1,6 +1,7 @@
 class Ant {
-  constructor(w, h, speed, radius) {
-    this.pos = createVector(random(0, w), random(0, h));
+  constructor(home, w, h, speed, radius) {
+    // this.pos = createVector(random(0, w), random(0, h));
+    this.pos = home;
     this.dir = p5.Vector.random2D().mult(speed);
     this.w = w;
     this.h = h;
@@ -19,41 +20,6 @@ class Ant {
       radius + 2
     );
     ellipse(this.pos.x - dir_vec.x, this.pos.y - dir_vec.y, radius, radius);
-  }
-
-  move(momentum, randomness, food_coeff, home_coeff, food) {
-    let directions = p5.Vector.mult(this.dir, momentum);
-    const dir_angle = this.dir.heading();
-    const random_angle = p5.Vector.fromAngle(
-      random(dir_angle - 1.5, dir_angle + 1.5)
-    ); // π/2 = 1.57079633
-    directions.add(p5.Vector.mult(random_angle, randomness));
-    if (this.has_food) {
-      directions.add(
-        p5.Vector.sub(home, this.pos).normalize().mult(home_coeff)
-      );
-    } else {
-      let food_dir = createVector(0, 0);
-      for (let i = 0; i < food.length; i++) {
-        const f = food[i];
-        const dist_dir = p5.Vector.sub(f, this.pos);
-        food_dir = p5.Vector.add(
-          food_dir,
-          dist_dir.mult(Math.exp(-0.1 * dist_dir.mag()))
-        );
-      }
-      if (food_dir.mag() > 1.0) {
-        food_dir.normalize();
-      }
-      directions.add(food_dir.mult(food_coeff));
-    }
-    const mov = directions.normalize().mult(this.speed);
-
-    let a = p5.Vector.add(this.pos, mov);
-    a.x = constrain(a.x, 0, this.w);
-    a.y = constrain(a.y, 0, this.h);
-    this.dir = p5.Vector.sub(a, this.pos).normalize();
-    this.pos = a;
   }
 
   is_home(home, home_radius) {
@@ -77,5 +43,166 @@ class Ant {
       }
     }
     return false;
+  }
+
+  home_dir(home) {
+    return p5.Vector.sub(home, this.pos).normalize();
+  }
+
+  food_dir(food) {
+    let food_dir = createVector(0, 0);
+    for (let i = 0; i < food.length; i++) {
+      const f = food[i];
+      const dist_dir = p5.Vector.sub(f, this.pos);
+      food_dir = p5.Vector.add(
+        food_dir,
+        dist_dir.mult(Math.exp(-0.1 * dist_dir.mag()))
+      );
+    }
+    if (food_dir.mag() > 1.0) {
+      food_dir.normalize();
+    }
+    return food_dir;
+  }
+
+  move(momentum, randomness, food_coeff, home_coeff, food) {
+    let directions = p5.Vector.mult(this.dir, momentum);
+    const dir_angle = this.dir.heading();
+    const random_angle = p5.Vector.fromAngle(
+      random(dir_angle - 1.5, dir_angle + 1.5)
+    ); // π/2 = 1.57079633
+    directions.add(p5.Vector.mult(random_angle, randomness));
+    if (this.has_food) {
+      directions.add(this.home_dir(home).mult(home_coeff));
+    } else {
+      let food_dir = this.food_dir(food);
+      directions.add(food_dir.mult(food_coeff));
+    }
+    const mov = directions.normalize().mult(this.speed);
+
+    let a = p5.Vector.add(this.pos, mov);
+    a.x = constrain(a.x, 0, this.w);
+    a.y = constrain(a.y, 0, this.h);
+    this.dir = p5.Vector.sub(a, this.pos).normalize();
+    this.pos = a;
+  }
+
+  move_pheromones(
+    momentum,
+    randomness,
+    food_coeff,
+    home_coeff,
+    food,
+    pheromones
+  ) {
+    let directions = p5.Vector.mult(this.dir, momentum);
+    const dir_angle = this.dir.heading();
+    const random_angle = p5.Vector.fromAngle(
+      random(dir_angle - Math.PI / 2, dir_angle + Math.PI / 2)
+    );
+    directions.add(p5.Vector.mult(random_angle, randomness));
+
+    const x = Math.floor(this.pos.x / pheromones.pheromones_resolution);
+    const y = Math.floor(this.pos.y / pheromones.pheromones_resolution);
+    const start_x = constrain(
+      x - Math.floor(pheromones.pheromones_range / 2),
+      0,
+      Math.ceil(this.w / pheromones.pheromones_resolution) - 1
+    );
+    const start_y = constrain(
+      y - Math.floor(pheromones.pheromones_range / 2),
+      0,
+      Math.ceil(this.h / pheromones.pheromones_resolution) - 1
+    );
+    const end_x = constrain(
+      x + Math.floor(pheromones.pheromones_range / 2),
+      0,
+      Math.ceil(this.w / pheromones.pheromones_resolution) - 1
+    );
+    const end_y = constrain(
+      y + Math.floor(pheromones.pheromones_range / 2),
+      0,
+      Math.ceil(this.h / pheromones.pheromones_resolution) - 1
+    );
+
+    if (this.has_food) {
+      // let home_dir = createVector(0, 0);
+      // for (let i = start_x; i <= end_x; i++) {
+      //   for (let j = start_y; j < end_y; j++) {
+      //     let dist_dir = p5.Vector.sub(
+      //       createVector(
+      //         i * pheromones.pheromones_resolution + pheromones_resolution / 2,
+      //         j * pheromones.pheromones_resolution + pheromones_resolution / 2
+      //       ),
+      //       this.pos
+      //     );
+      //     if (
+      //       dist_dir.heading() > ((dir_angle - Math.PI / 4) % 2) * Math.PI &&
+      //       dist_dir.heading() < ((dir_angle + Math.PI / 4) % 2) * Math.PI
+      //     ) {
+      //       home_dir.add(
+      //         dist_dir.mult(pheromones.pheromones_ttl - pheromones.home_pheromones[i][j])
+      //       );
+      //     }
+      //   }
+      // }
+      // directions.add(p5.Vector.mult(home_dir.normalize(), home_coeff));
+      directions.add(this.home_dir(home).mult(home_coeff));
+    } else {
+      let food_dir = createVector(0, 0);
+      for (let i = start_x; i <= end_x; i++) {
+        for (let j = start_y; j < end_y; j++) {
+          let dist_dir = p5.Vector.sub(
+            createVector(
+              i * pheromones.pheromones_resolution + pheromones_resolution / 2,
+              j * pheromones.pheromones_resolution + pheromones_resolution / 2
+            ),
+            this.pos
+          );
+          if (
+            dist_dir.heading() > ((dir_angle - Math.PI / 4) % 2) * Math.PI &&
+            dist_dir.heading() < ((dir_angle + Math.PI / 4) % 2) * Math.PI &&
+            pheromones.food_pheromones[i][j] > 2
+          ) {
+            food_dir.add(
+              dist_dir
+                .normalize()
+                .mult(
+                  pheromones.pheromones_ttl - pheromones.food_pheromones[i][j]
+                )
+            );
+            // food_dir.normalize().add(dist_dir);
+          }
+        }
+      }
+      directions.add(p5.Vector.mult(food_dir, food_coeff));
+      directions.add(p5.Vector.mult(this.food_dir(food), 0.2));
+    }
+
+    const mov = directions.normalize().mult(this.speed);
+
+    let a = p5.Vector.add(this.pos, mov);
+    a.x = constrain(a.x, 0, this.w);
+    a.y = constrain(a.y, 0, this.h);
+    this.dir = p5.Vector.sub(a, this.pos).normalize();
+    this.pos = a;
+  }
+
+  emit_pheromones(pheromones) {
+    const i = constrain(
+      Math.floor(this.pos.x / pheromones.pheromones_resolution),
+      0,
+      Math.ceil(this.w / pheromones.pheromones_resolution) - 1
+    );
+    const j = constrain(
+      Math.floor(this.pos.y / pheromones.pheromones_resolution),
+      0,
+      Math.ceil(this.h / pheromones.pheromones_resolution) - 1
+    );
+    if (this.has_food) {
+      pheromones.food_pheromones[i][j] = pheromones.pheromones_ttl;
+    } else {
+      pheromones.home_pheromones[i][j] = pheromones.pheromones_ttl;
+    }
   }
 }
--- js/main.js
+++ js/main.js
@@ -19,15 +19,50 @@ const ant_number = 50;
 const ant_radius = 8;
 const ant_color = "#572D15";
 const ant_stroke = "#572D15";
-const ant_speed = 3;
+const ant_food_color = "#B64D3A";
+const ant_speed = 4;
 
-const momentum = 0.6; // memory
-const randomness = 0.15;
-const home_coeff = 0.25;
-const food_coeff = 0.25;
+const do_pheromones = false;
+
+let momentum = 0.6; // memory
+let randomness = 0.15;
+let home_coeff = 0.25;
+let food_coeff = 0.25;
+
+if (do_pheromones) {
+  momentum = 0.7;
+  randomness = 0.2;
+  home_coeff = 0.2;
+  food_coeff = 0.1;
+}
 
 let food_source;
 let ants = [];
+
+const pheromones_range = 15;
+const pheromones_resolution = 10;
+const pheromones_ttl = 24000;
+
+let home_pheromones = [];
+let food_pheromones = [];
+for (let i = 0; i < Math.ceil(w / pheromones_resolution); i++) {
+  let row_h = [];
+  let row_f = [];
+  for (let j = 0; j < Math.ceil(h / pheromones_resolution); j++) {
+    row_h.push(0);
+    row_f.push(0);
+  }
+  home_pheromones.push(row_h);
+  food_pheromones.push(row_f);
+}
+
+const pheromones = {
+  pheromones_resolution,
+  pheromones_range,
+  pheromones_ttl,
+  home_pheromones,
+  food_pheromones,
+};
 
 const home_radius = 25;
 const home_color = "#B64D3A";
@@ -37,10 +72,11 @@ function setup() {
   createCanvas(w, h);
   frameRate(fr);
   textSize(30);
-  for (let index = 0; index < ant_number; index++) {
-    ants.push(new Ant(w, h, ant_speed, ant_radius));
-  }
+
   home = createVector(random(0, w), random(0, h));
+  for (let index = 0; index < ant_number; index++) {
+    ants.push(new Ant(home, w, h, ant_speed, ant_radius));
+  }
   // food_source = new FoodSource(w, h, food_quantity, food_rate, food_refill);
   food_source = new FoodSourceConcentrated(
     w,
@@ -59,6 +95,10 @@ function draw() {
   // Draw
   background(background_color);
 
+  if (do_pheromones) {
+    draw_pheromones();
+  }
+
   // Food
   fill(food_color);
   stroke(food_stroke);
@@ -71,16 +111,30 @@ function draw() {
   stroke(ant_stroke);
   for (let i = 0; i < ants.length; i++) {
     let ant = ants[i];
-    ant.move(momentum, randomness, food_coeff, home_coeff, food_source.food);
+    if (do_pheromones) {
+      ant.move_pheromones(
+        momentum,
+        randomness,
+        food_coeff,
+        home_coeff,
+        food_source.food,
+        pheromones
+      );
+    } else {
+      ant.move(momentum, randomness, food_coeff, home_coeff, food_source.food);
+    }
     const f = ant.get_food(food_source.food, food_radius);
     if (f) {
-      fill(home_color);
+      fill(ant_food_color);
     } else {
       fill(ant_color);
     }
     tot_food += ant.is_home(home, home_radius);
+    if (do_pheromones) {
+      ant.emit_pheromones(pheromones);
+      evaporate();
+    }
     ant.draw(ant_radius);
-    // ellipse(ant.pos.x, ant.pos.y, ant_radius, ant_radius);
   }
 
   // Home
@@ -98,4 +152,57 @@ function draw() {
   fill(255);
   stroke(0);
   text("FOOD: " + tot_food, 10, 40);
+}
+
+function evaporate() {
+  for (let i = 0; i < Math.ceil(w / pheromones_resolution); i++) {
+    for (let j = 0; j < Math.ceil(h / pheromones_resolution); j++) {
+      if (pheromones.home_pheromones[i][j] > 0) {
+        pheromones.home_pheromones[i][j]--;
+      }
+      if (pheromones.food_pheromones[i][j] > 0) {
+        pheromones.food_pheromones[i][j]--;
+      }
+    }
+  }
+}
+
+const food_pher_col = "rgba(255,113,113,";
+const home_pher_col = "rgba(159,216,223,";
+
+function draw_pheromones() {
+  for (let i = 0; i < Math.ceil(w / pheromones_resolution); i++) {
+    for (let j = 0; j < Math.ceil(h / pheromones_resolution); j++) {
+      // if (pheromones.home_pheromones[i][j] > 0) {
+      //   fill(
+      //     home_pher_col +
+      //       (
+      //         pheromones.home_pheromones[i][j] / pheromones.pheromones_ttl
+      //       ).toFixed(2) +
+      //       ")"
+      //   );
+      //   ellipse(
+      //     i * pheromones_resolution + pheromones_resolution / 2,
+      //     j * pheromones_resolution + pheromones_resolution / 2,
+      //     pheromones_resolution / 2,
+      //     pheromones_resolution / 2
+      //   );
+      // }
+      if (pheromones.food_pheromones[i][j] > 0) {
+        fill(
+          food_pher_col +
+            (
+              pheromones.food_pheromones[i][j] / pheromones.pheromones_ttl
+            ).toFixed(2) +
+            ")"
+        );
+        ellipse(
+          i * pheromones_resolution + pheromones_resolution / 2,
+          j * pheromones_resolution + pheromones_resolution / 2,
+          pheromones_resolution / 2,
+          pheromones_resolution / 2
+        );
+      }
+    }
+  }
 }
